8daaa6981ad4b23e492a4e2e9eec4bf2ed9d5ea6
Catalin Palade <catalin@recruitd.com>
2019-10-31 11:17:00 +0200

    google places api in place and working

--- PlacesDB/ViewController.swift
+++ PlacesDB/ViewController.swift
@@ -7,14 +7,78 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var arrayCities = [GMSAutocompletePrediction]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    lazy var filter: GMSAutocompleteFilter = {
+        let filter = GMSAutocompleteFilter()
+        filter.type = .city
+        return filter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loadUI()
     }
 
+    func loadUI() {
+        tableView.tableFooterView = UIView()
+    }
 
 }
 
+// MARK: UISearchBarDelegate
+extension ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText == "" {
+            arrayCities = [GMSAutocompletePrediction]()
+        } else {
+            GMSPlacesClient.shared().autocompleteQuery(searchText, bounds: nil, filter: filter) { (result, error) in
+                if error == nil {
+                    if let res = result {
+                        self.arrayCities = res
+                    }
+                }
+            }
+        }
+    }
+}
+
+// MARK: UITableViewDelegate
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        arrayCities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        }
+        cell?.textLabel?.attributedText = arrayCities[indexPath.row].attributedFullText
+        cell?.textLabel?.font = UIFont.systemFont(ofSize: 14)
+        
+        cell?.selectionStyle = .none
+        
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(tableView.cellForRow(at: indexPath)?.textLabel?.text ?? "")
+        searchBar.text? = ""
+        arrayCities = [GMSAutocompletePrediction]()
+    }
+    
+}
