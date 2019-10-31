16451332df411adb6b5fb3b08466497e84ce930f
Catalin Palade <catalin@recruitd.com>
2019-10-31 10:08:03 +0200

    Initial Commit

---
+++ PlacesDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlacesDB
+//
+//  Created by Catalin Palade on 31/10/2019.
+//  Copyright © 2019 Catalin Palade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
